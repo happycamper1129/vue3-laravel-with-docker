d9e4b3f82a1b0d37a1f90d6a82703c8a208bb6bd
happycamper <happycamper.sw@gmail.com>
2023-01-01 05:15:06 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
-   Route::get('user', [AuthController::class, 'user']);
+   Route::get('/user', [AuthController::class, 'user']);
+   Route::post('/logout', [AuthController::class, 'logout']);
 });
